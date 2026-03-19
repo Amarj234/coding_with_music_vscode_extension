59205445e08bc5be594c7eba63a7b7ce788e005b
Amarj234 <amarj234@gmail.com>
2026-03-19 22:51:37 +0530

    fix: remove CSP meta tag to test playback

--- src/extension.ts
+++ src/extension.ts
@@ -101,7 +101,6 @@ class CodingMusicViewProvider implements vscode.WebviewViewProvider {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Security-Policy" content="default-src * 'unsafe-inline' 'unsafe-eval'; frame-src *; img-src https: data: vscode-resource:; media-src https: data: blob:; connect-src https: wss:;">
     <title>Coding Music Player</title>
     <style>
         html, body {
